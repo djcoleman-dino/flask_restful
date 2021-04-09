d4ea76eac717f278c3c10cf4581c382e9edac73b
DJ Coleman <DJ@DJs-MacBook-Pro.attlocal.net>
2021-04-08 19:15:46 -0700

    Added postgres

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 # source venv/bin/activate - activate virtual env
+import os
+
 from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT
@@ -11,12 +13,13 @@ from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
+    'DATABASE_URL', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_NOTIFICATIONS'] = False
 app.secret_key = 'billy'
 api = Api(app)
 
-jwt = JWT(app, authenticate, identity)
+jwt = JWT(app, authenticate, identity)  # /auth
 
 api.add_resource(Store, '/store/<string:name>')
 api.add_resource(Item, '/item/<string:name>')
